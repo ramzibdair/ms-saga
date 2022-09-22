10d477478037e57cb32de29a2070f788d5241984
ramzi <ramzi@LAPTOP-MVODADDD>
2022-09-22 15:40:38 +0300

    intial

---
+++ src/ms-saga/BuildingBlock/Saga.Machine/Program.cs
@@ -0,0 +1,10 @@
+using Saga.Machine;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
