4c44a30912e90499f093655f6ca9f5597589ebd8
ramzi <ramzi@LAPTOP-MVODADDD>
2022-09-24 15:15:33 +0300

    add contracts of saga

---
+++ src/ms-saga/BuildingBlock/EventBus.Messages/Configuration/BusConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Configuration
+{
+    public class BusConstants
+    {
+        public const string RabbitMqUri = "rabbitmq://localhost/";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string validateOrderQueue = "validate-order-queue";
+        public const string StartOrderTranastionQueue = "start-order-queue";
+    }
+}
